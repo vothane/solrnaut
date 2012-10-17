17ead245fe6ce1d87d2ed290b5bd7881feea6e6c
Thane Vo <vothane@gmail.com>
2012-10-17 14:37:01 -0600

    TDD R GTFO

---
+++ spec/solrnaut_spec.rb
@@ -0,0 +1,19 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+ActiveRecord::Base.send :include, Sunspot::Rails::Searchable
+
+SearchDelegator < ActiveRecord::Base
+
+end
+
+SearchProxy
+  search_on :SearchDelegator
+end
+
+describe Solrnaut do
+
+  it 'have searchable method proxy'
+    
+  end
+
+End
---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+require 'active_record' 
+require 'sunspot_rails'
+$:.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
+require 'solrnaut'
+
+RSpec.configure do |config|
+  # Use color in STDOUT
+  config.color_enabled = true
+
+  # Use color not only in STDOUT but also in pagers and files
+  config.tty = true
+
+  # Use the specified formatter
+  config.formatter = :documentation # :progress, :html, :textmate
+end
